09a74ea2c12a64c66c8ea6e9888d583206204033
sixminutestogg <yangyang.zhang1@newtouch.com>
2020-10-13 17:01:08 +0800

    Initial commit

---
+++ src/test/java/cn/com/zhang/rabbitmqspringboot/TestRabbitmq.java
@@ -0,0 +1,15 @@
+package cn.com.zhang.rabbitmqspringboot;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = RabbitmqSpringbootApplication.class)
+@RunWith(SpringRunner.class)
+public class TestRabbitmq {
+
+
+
+
+
+}
